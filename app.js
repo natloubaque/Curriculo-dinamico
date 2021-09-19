4d4bda07c038e7d3032a4d922266f41e3515c10d
natloubaque <89233390+natloubaque@users.noreply.github.com>
2021-09-19 18:09:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+const homeStartingContent = "#home";
+const experienceContent = "#experience";
+const aboutContent = "#about";
+const habilityContent = "#hability";
+const contactContent = "#contact" ;
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      });
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
